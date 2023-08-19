3d06900044d50d4228c0ba401c2bdd55a7608508
shenal <shenal@umich.edu>
2023-08-19 14:11:00 -0400

    Added chrome storage api

--- popup.js
+++ popup.js
@@ -86,3 +86,21 @@ function goToAnotherPage() {
         window.location.href = "/Timer/timer.html"; // Replace with your target page URL
     }, 500); // Wait for the animation to finish before navigating
 }
+
+const noteTextarea = document.getElementById('note');
+const saveButton = document.getElementById('save');
+
+// Load saved note on popup open
+chrome.storage.local.get('note', (data) => {
+  if (data.note) {
+    noteTextarea.value = data.note;
+  }
+});
+
+// Save note on button click
+saveButton.addEventListener('click', () => {
+  const note = noteTextarea.value;
+  chrome.storage.local.set({ 'note': note }, () => {
+    console.log('Note saved: ' + note);
+  });
+})
